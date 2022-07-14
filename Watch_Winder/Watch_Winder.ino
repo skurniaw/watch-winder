8e4aa54c9bbb0ed13191d56cd927881994551841
Sebastian K <48970911+skurniaw@users.noreply.github.com>
2022-07-14 00:49:06 -0700

    Revise logic and comments
    calcUltimateMin method was unnecessary. This has been replaced with calcMinOfDay.
    calcSteps method added, to provide simple conversion of a desired number of rotations to stepper motor steps.
    
    Revised comments for public methods.

--- Watch_Winder/Watch_Winder.ino
+++ Watch_Winder/Watch_Winder.ino
@@ -1,11 +1,18 @@
-// Sebastian Kurniawan, Arduino 1.8.2
-// DIY Watch Winder, 14 Jul 2019
-// v 1.4
+/*
+Sebastian Kurniawan, Arduino 1.8.9
+DIY Watch Winder, 12 June 2022
+v 2.0
 
-// This program will turn a stepper motor a specified amount, after which it will
-// stop, delay itself for a specified time, then turn the stepper motor the same
-// amount in the opposite direction. Will continue indefinitely. Requires a DS 3231
-// real-time clock and a stepper motor.
+This program will turn a stepper motor a specified number of rotations, after which it 
+will stop, delay itself for a specified number of minutes, then turn the stepper motor the 
+same number of rotations in the opposite direction. Will continue indefinitely. 
+Requires a DS 3231 real-time clock and a stepper motor. 
+
+Libraries used:
+Patrick Wasp's AccelStepper library.
+RinkyDinkElectronic's DS3231 library.
+
+*/
 
 #include <AccelStepper.h>
 #include <DS3231.h> // Real-time clock library.
@@ -24,69 +31,67 @@ Time  t;  // Time object t. Used to return date/hour/minute/sec etc.
 // Initialize with pin sequence IN1-IN3-IN2-IN4 for using the AccelStepper with 28BYJ-48
 AccelStepper stepper1(HALFSTEP, motorPin1, motorPin3, motorPin2, motorPin4);
 
+// Modify these to change how long to wait between rotating, and how many rotations desired
+int pauseMins = 30;
+int rotations = 15;
 
 long initMin = 0;
 long currentMin = 0;
-
-int daysPerMonth[] = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
-String monthNames[] = {"January" "February" "March" "April" "May" "June" "July" "August" "September" "October" "November" "December"};
-
-long spinDirection = -80000;
-
 boolean spin = true;
+long destinationPosition = 0;
 
 void setup() {
-  //  stepper1.setSpeed(200);
-  stepper1.setMaxSpeed(3000.0);
-  stepper1.setAcceleration(300.0);
-
-  stepper1.moveTo(-spinDirection);
-  Serial.begin(9600);
+  stepper1.setMaxSpeed(750.0);
+  stepper1.setAcceleration(100.0);
+  destinationPosition = calcSteps(rotations);
+  stepper1.move(destinationPosition);
   rtc.begin();
 
-  t = rtc.getTime();
-
 // Uncomment to set the date and time of the real-time clock.
-//  rtc.setDOW(MONDAY);     // Set Day-of-Week to SUNDAY
-//  rtc.setTime(18, 34, 0);     // Set the time to 12:00:00 (24hr format)
-//  rtc.setDate(3, 9, 2018);   // day month year
+//  rtc.setDOW(TUESDAY);     // Set Day-of-Week to SUNDAY
+//  rtc.setTime(12, 04, 0);     // Set the time to 12:00:00 (24hr format)
+//  rtc.setDate(1, 2, 2022);   // day month year
 
-}//--(end setup )---
+}
 
 void loop() {
-  //Change direction when the stepper reaches the target position
   t = rtc.getTime();
-  currentMin = calcUltimateMin(rtc.getMonthStr(), t.date, t.hour, t.min);
-  
-  if (currentMin > (initMin + 60)){
+  currentMin = calcMinOfDay(t.hour, t.min);
+
+  if (currentMin < initMin && (1440 - initMin) + currentMin >= pauseMins) {
+    // Overlapped into a new day
+    stepper1.run();
+  } else if (currentMin >= (initMin + pauseMins)) {
     stepper1.run();
   }
 
   if (stepper1.distanceToGo() == 0) {
-    spinDirection = spinDirection * -1;
-    stepper1.moveTo(spinDirection);
-    initMin = calcUltimateMin(rtc.getMonthStr(), t.date, t.hour, t.min);
+    destinationPosition = -destinationPosition;
+    stepper1.move(destinationPosition);
+    initMin = calcMinOfDay(t.hour, t.min);
   }
 }
 
-
-// Calculates ultimate minute in a year using real-time clock data. Returns minute as a long.
-long calcUltimateMin(String month, int days, int hr, int minute) {
-  int ultDays = 0;
-  int monthNum = getMonth(month);
-  for (int i = 0; i < monthNum; i++) {
-    ultDays += daysPerMonth[i];
-  }
-  ultDays += (days - 1); // e.g. for the 2nd @ 3am, it will be 1 day elapsed plus 3 hours
-  return (ultDays * 24L) * 60 + (hr * 60) + minute;
+/**
+ * Returns an integer representing the minute of the day.
+ * 
+ * @param hr      an integer hour
+ * @param minute  an integer minute
+ * @return        the minute of the day corresponding to the hour and minute passed
+ */
+int calcMinOfDay(int hr, int minute) {
+  return (hr * 60) + minute;
 }
 
-// Takes a string specifying the month and returns a integer corresponding to each month.
-// January = 0, February = 1, ... , December = 11
-int getMonth(String month) {
-  for (int i = 0; i < 11; i++) {
-    if (month == monthNames[i]) {
-      return i;
-    }
-  }
+/**
+ * Returns a long representing the number of steps the stepper motor must take for a
+ * certain number of rotations.
+ * 
+ * @param rotations   integer of desired number of rotations for the watch winder between
+ *                    pauses
+ * @return            long of required steps corresponding to the rotations
+ */
+long calcSteps(int rotations) {
+  long steps = (long) rotations * 4100;
+  return steps;
 }
